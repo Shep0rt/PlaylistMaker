93470e2096c8628e8cfebb68627d8f0d2d2ed8cc
Pavel <pavel.michka@gmail.com>
2025-07-17 02:39:52 +0200

    Исправил замечания

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -2,13 +2,31 @@ package com.practicum.playlistmaker
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rootView = findViewById<View>(R.id.root_layout)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { view, insets ->
+            val statusBarInsets = insets.getInsets(WindowInsetsCompat.Type.statusBars())
+            val navBarInsets = insets.getInsets(WindowInsetsCompat.Type.navigationBars())
+            view.updatePadding(
+                top = statusBarInsets.top,
+                bottom = navBarInsets.bottom
+            )
+            insets
+        }
 
         val buttonSearch = findViewById<Button>(R.id.btn_search)
         val buttonMediaLibrary = findViewById<Button>(R.id.btn_media_library)
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,18 +1,32 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
-import android.widget.ImageButton
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        //Действия при клике на кнопку "Назад" внутри раздела "Настройки"
-        val backButton = findViewById<ImageButton>(R.id.back_button)
+        val rootView = findViewById<View>(R.id.root_layout)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { view, insets ->
+            val statusBarInsets = insets.getInsets(WindowInsetsCompat.Type.statusBars())
+            view.updatePadding(top = statusBarInsets.top)
+            insets
+        }
 
-        backButton.setOnClickListener {
+        //Действия при клике на кнопку "Назад" внутри раздела "Настройки"
+        val toolbar = findViewById<MaterialToolbar>(R.id.settings_toolbar)
+
+        toolbar.setNavigationOnClickListener {
             finish()
         }
     }
