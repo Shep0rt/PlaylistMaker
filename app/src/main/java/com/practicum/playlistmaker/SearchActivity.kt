510e087c812db9e92ac1ccaeef8a70eaf67af213
Pavel <pavel.michka@gmail.com>
2025-07-26 01:47:58 +0200

    Спринт №9 - Создал общий стиль для атрибутов на главном экране и в настройках - Убрал тени с кнопок - Сделал адаптацию экранов под темный режим - Реализовал кнопки на экране "Настройки": «Поделиться приложением», «Написать разработчикам», «Пользовательское соглашение» - Реализовал экран "Поиск" и функционал поля поиска на нем - Реализовал сохранение и восстановление данных в поле поиска

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,11 +1,91 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import com.google.android.material.appbar.MaterialToolbar
 
 class SearchActivity : AppCompatActivity() {
+    private var searchText: String = SEARCH_TEXT_DEF
+    companion object {
+        const val SEARCH_TEXT = "SEARCH_TEXT"
+        const val SEARCH_TEXT_DEF = ""
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val rootView = findViewById<View>(R.id.root_layout)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { view, insets ->
+            val statusBarInsets = insets.getInsets(WindowInsetsCompat.Type.statusBars())
+            val navBarInsets = insets.getInsets(WindowInsetsCompat.Type.navigationBars())
+            view.updatePadding(
+                top = statusBarInsets.top,
+                bottom = navBarInsets.bottom
+            )
+            insets
+        }
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.search_toolbar)
+        val searchEditText = findViewById<EditText>(R.id.search_edit_text)
+        val clearButton = findViewById<ImageView>(R.id.clear_button)
+
+        //Действия при клике на кнопку "Назад" внутри раздела "Поиск"
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+        //Работа поля поиска
+        //Дейтсвие при клике на кнопку "Х" внутри поля поиска
+        clearButton.setOnClickListener {
+            searchEditText.text.clear()
+        }
+
+        //Действия с текстом внутри поля поиска
+        val searchTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                searchText = s?.toString() ?: SEARCH_TEXT_DEF
+            }
+        }
+        searchEditText.addTextChangedListener(searchTextWatcher)
+    }
+
+    //Сохраняем внесенные изменения пользователем, на данной activity
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("SEARCH_TEXT", searchText)
+    }
+
+    //Восстанавливаем внесенные изменения пользователем, на данной activity
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchText = savedInstanceState.getString(SEARCH_TEXT, SEARCH_TEXT_DEF)
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,13 +1,18 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.textview.MaterialTextView
+import androidx.core.net.toUri
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,11 +28,49 @@ class SettingsActivity : AppCompatActivity() {
             insets
         }
 
-        //Действия при клике на кнопку "Назад" внутри раздела "Настройки"
         val toolbar = findViewById<MaterialToolbar>(R.id.settings_toolbar)
+        val shareTextView = findViewById<MaterialTextView>(R.id.share_text)
+        val supportTextView = findViewById<MaterialTextView>(R.id.support)
+        val userAgreementTextView = findViewById<MaterialTextView>(R.id.user_agreement)
 
+        //Действия при клике на кнопку "Назад" внутри раздела "Настройки"
         toolbar.setNavigationOnClickListener {
             finish()
+        }
+
+        //Действия при клике на кнопку "Поделится приложением" внутри раздела "Настройки"
+        shareTextView.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.share_app_text))
+            }
+            startActivity(Intent.createChooser(intent, getString(R.string.share_app_title)))
+        }
+
+        //Действия при клике на кнопку "Написать в поддержку" внутри раздела "Настройки"
+        supportTextView.setOnClickListener {
+            val email = "mailto:" + Uri.encode(getString(R.string.email_address)) +
+                    "?subject=" + Uri.encode(getString(R.string.support_subject)) +
+                    "&body=" + Uri.encode(getString(R.string.support_body))
+
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = email.toUri()
+            }
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, getString(R.string.error_not_app_email), Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        //Действия при клике на кнопку "Пользовательское соглашение" внутри раздела "Настройки"
+        userAgreementTextView.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, getString(R.string.practicum_offer).toUri())
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, getString(R.string.error_not_app_link), Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
